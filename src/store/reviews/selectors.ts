a24791c6248cb6c344296065fab5f5abcd192095
Stepan Vasilyev <intellectualspecialist@gmail.com>
2026-04-23 02:21:44 +0500

    fix: меняет toSorted на sort по проекту

--- src/store/reviews/selectors.ts
+++ src/store/reviews/selectors.ts
@@ -4,7 +4,7 @@ import { State } from '../../types/state-type';
 
 export const selectReviews = createSelector(
   [(state: Pick<State, NameSpace.Reviews>) => state[NameSpace.Reviews].reviews],
-  (reviews) => reviews.toSorted((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
+  (reviews) => [...reviews].sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
 );
 export const selectReviewsStatus = (state: Pick<State, NameSpace.Reviews>) => state[NameSpace.Reviews].reviewsStatus;
 export const selectReviewStatus = (state: Pick<State, NameSpace.Reviews>) => state[NameSpace.Reviews].reviewStatus;
--- src/utils/sorting.ts
+++ src/utils/sorting.ts
@@ -7,11 +7,11 @@ const sortOffers = (sorting: SortingOptionType, offers: Offer[]): Offer[] => {
     case SortingOption[0]:
       return offers;
     case SortingOption[1]:
-      return offers.toSorted((a, b): number => Number(a.price) - Number(b.price));
+      return [...offers].sort((a, b): number => Number(a.price) - Number(b.price));
     case SortingOption[2]:
-      return offers.toSorted((a, b): number => Number(b.price) - Number(a.price));
+      return [...offers].sort((a, b): number => Number(b.price) - Number(a.price));
     case SortingOption[3]:
-      return offers.toSorted((a, b): number => Number(b.rating) - Number(a.rating));
+      return [...offers].sort((a, b): number => Number(b.rating) - Number(a.rating));
     default:
       return offers;
   }
